3995181a09e153cf915a483b988aa8c4a4923b09
Edwin Chan <edwinchan@u.nus.edu>
2017-03-12 08:10:28 +0800

    minor fixes

--- r_code/headsupcomparison.R
+++ r_code/headsupcomparison.R
@@ -3,11 +3,12 @@ df <- read.csv(file="C:\\Users\\User\\Documents\\thesis\\r_code\\headsupcomparis
 
 df$Name <- factor(df$Name, levels = df$Name[order(df$Order)])
 
-d<- ggplot(df, aes(x=Name, y=Score)) +
-  geom_bar(stat="identity",aes(fill=Type)) +
-  scale_fill_discrete(name="Game") + xlab("") + ylab("F1 Score") + theme(axis.text.x = element_text(angle = 90, hjust = 1))+
-  ggtitle("Final F1 Score Differences") +
-  theme(plot.title = element_text(family = "Trebuchet MS", color="black", face="bold", size=16, hjust=0.5)) 
+d <- ggplot(df, aes(x=Name, y=Score))+
+  geom_bar(stat="identity",aes(fill=Name),width=0.5) + facet_grid(.~Type) +
+  coord_cartesian(ylim=c(0, 1)) + scale_y_continuous(breaks=waiver()) +
+  scale_fill_discrete(name="Game") + xlab("") + ylab("Score") + theme(axis.text.x = element_text(angle = 90, hjust = 1))+ ggtitle("Comparison of Best Calling Methods") +
+  theme(plot.title = element_text(family = "Trebuchet MS", color="black", face="bold", size=16, hjust=0.5)) + coord_equal(1/0.2) 
+
 d
 
 ggsave("C:\\Users\\User\\Documents\\thesis\\r_code\\headsupcomparison.jpg")
